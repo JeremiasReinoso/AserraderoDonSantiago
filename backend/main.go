7f83180ebc36faa8276d00583ec612660056b4ba
JeremiasReinoso <141459619+JeremiasReinoso@users.noreply.github.com>
2026-05-27 20:19:17 -0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
-	"net/https:aserraderodonsantiago.com.ar"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
 
+	// Puerto que usa Render
 	port := os.Getenv("PORT")
 
+	// Si no existe, usa 8080 localmente
 	if port == "" {
 		port = "8080"
 	}
 
+	// Carpeta pública del sitio
 	fs := http.FileServer(http.Dir("./"))
 
+	// Mostrar archivos estáticos
 	http.Handle("/", fs)
 
-	http.ListenAndServe(":"+port, nil)
+	log.Println("Servidor iniciado en puerto:", port)
+
+	// Iniciar servidor
+	err := http.ListenAndServe(":"+port, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
