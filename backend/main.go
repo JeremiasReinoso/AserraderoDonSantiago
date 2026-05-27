956a661ad039ed9295a6f9cf9c144b4d81deae0d
JeremiasReinoso <141459619+JeremiasReinoso@users.noreply.github.com>
2026-05-27 19:45:08 -0300

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	fs := http.FileServer(http.Dir("./"))
+
+	http.Handle("/", fs)
+
+	http.ListenAndServe(":"+port, nil)
+}
